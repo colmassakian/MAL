9a2905443a4652dbb384747b638cc30fc02c8036
colmassakian <43759428+colmassakian@users.noreply.github.com>
2020-09-28 23:52:44 -0700

    Remove extra phrases from title before searching

--- content.js
+++ content.js
@@ -1,9 +1,26 @@
 /* Listen for messages */
+function modifyTitle(title) {
+  // Remove any item in () except years
+  title = title.replace(" (Dub)", "");
+  title = title.replace(" (Sub)", "");
+  title = title.replace(" (TV)", "");
+
+  // Remove parentheses around years
+  title = title.replace("(", "");
+  title = title.replace(")", "");
+
+  return title;
+}
+
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
   /* If the received message has the expected format... */
   if (msg.text && (msg.text == "report_back")) {
       /* Call the specified callback, passing 
          the web-pages DOM content as argument */
-    sendResponse(document.getElementsByClassName("jtitle")[0].innerHTML);
+    // TODO: Remove dub from title
+    // var title = document.getElementsByClassName("jtitle")[0].innerHTML
+    // title = title.replace(" (Dub)", "");
+    var title = modifyTitle(document.getElementsByClassName("jtitle")[0].innerHTML);
+    sendResponse(title);
   }
 });
